8e0c625364e4a24eabba69b0c7f6224694fc1e1c
wkpc <145634104+wkpc@users.noreply.github.com>
2025-02-15 12:36:49 -0700

    - added generateBoard(), boardLength(), boardHeight(), and generateStartEnd() functions

---
+++ src/Board.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+public class Board
+{
+    /**
+     * creates an empty playing board
+     *
+     * @param rows number of rows in the board
+     * @param columns number of columns in the board
+     * @return the generated board
+     */
+    public static int[][] generateBoard(int rows, int columns)
+    {
+        //create an empty board
+        int[][] board = new int[rows][columns];
+
+        return board;
+    }
+
+    /**
+     * gives the number of columns in the board
+     *
+     * @param board the board to be measured
+     * @return the number of columns in board
+     */
+    public static int boardLength(int[][] board)
+    {
+        return board[0].length;
+    }
+
+    /**
+     * gives the number of rows in the board
+     *
+     * @param board the board to be measured
+     * @return the number of rows in board
+     */
+    public static int boardHeight(int[][] board)
+    {
+        return board.length;
+    }
+
+    /**
+     * creates the start and end islands
+     *
+     * @param board      the playing board to add these islands to
+     * @param xBorder    the distance between the islands and the horizontal edges of the map
+     * @param yBorder    the distance between the islands and the vertical edges of the map
+     * @param islandSize size of the island
+     * @return the board with start and end islands added
+     */
+    public static void generateStartEnd(int[][] board, int xBorder, int yBorder, int islandSize)
+    {
+        //create instance of random class for number generation
+        Random rand = new Random();
+
+        //create random coordinates for the starting island, use island's top left corner to position
+        int startX = xBorder; //start island x pos is not randomized
+        int startY = yBorder + rand.nextInt(boardHeight(board) - yBorder);
+
+
+        //create random coordinates for the starting island, use island's top left corner to position
+        int endX = boardLength(board) - xBorder - 1; //end island x pos is not randomized
+        int endY = yBorder + rand.nextInt(boardHeight(board) - yBorder);
+
+        //add in islands of appropriate size
+        for (int l = 0; l < islandSize; l++)
+        {
+            board[startY + l][startX + l] = 0;
+            board[endY + l][endX + l] = 0;
+        }
+    }
+
+}
