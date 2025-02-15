6dee3cbd3e4dff40910beb6b06f35bf811b53448
Silver <jhfan0716@gmail.com>
2025-02-15 16:15:30 -0700

    Linked it to game.java, can now spawn in islands

--- src/Graphic.java
+++ src/Graphic.java
@@ -9,19 +9,27 @@ import java.awt.event.ActionListener;
 
 public class Graphic implements ActionListener {
     //Global variables
-    public static int rows = 10;
-    public static int columns = 10;
-    public static JButton[][] board = new JButton[rows][columns];
+    private int rows;
+    private int columns;
+    private int[][] gameBoard;
+    private JButton[][] board;
     //Colors
-    public static Color white = new Color(255, 255, 255);
-    public static Color red = new Color(255, 0, 0);
-    public static Color blue = new Color(0, 0, 255);
+    private final Color white = new Color(255, 255, 255);
+    private final Color red = new Color(200, 30, 0);
+    private final Color blue = new Color(0, 0, 220);
+    private final Color brown = new Color(150, 75, 0);
+    private final Color yellow = new Color(200, 200, 0);
+    private final Color grey = new Color(128, 128, 128);
 
-    public Graphic() {
+    public Graphic(int rows, int columns, int[][] gameBoard) {
+        this.rows = rows;
+        this.columns = columns;
+        this.board = new JButton[rows][columns];
+        this.gameBoard = gameBoard;
         //JFrame setup, first time doing this idk what they do
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000,1000);
+        frame.setSize(2000,1000);
         //Sets the grid layout of the frame to the number of indicated rows and columns
         frame.setLayout(new GridLayout(rows,columns));
         frame.setTitle("Game");
@@ -29,10 +37,25 @@ public class Graphic implements ActionListener {
         //initializes the JButtons which will be the grids for the board
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
+
                 board[i][j] = new JButton("");
-                board[i][j].setBackground(white);
                 board[i][j].setUI(new BasicButtonUI());
-                board[i][j].addActionListener(this);
+                if(gameBoard[i][j] == 0) {
+                    board[i][j].setBackground(white);
+                    board[i][j].addActionListener(this);
+                }else if(gameBoard[i][j] == 1){
+                    board[i][j].setBackground(red);
+                    board[i][j].setBorder(BorderFactory.createLineBorder(red));
+                }else if(gameBoard[i][j] == 2) {
+                    board[i][j].setBackground(blue);
+                    board[i][j].setBorder(BorderFactory.createLineBorder(blue));
+                }else if(gameBoard[i][j] == 3) {
+                    board[i][j].setBackground(grey);
+                    board[i][j].setBorder(BorderFactory.createLineBorder(grey));
+                }else{
+                    board[i][j].setBackground(white);
+                }
+
             }
         }
 
@@ -46,10 +69,6 @@ public class Graphic implements ActionListener {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Graphic();
-    }
-
     //Effects of what button presses do
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -58,6 +77,7 @@ public class Graphic implements ActionListener {
                 if (board[i][j].getModel().isRollover()) {
                     board[i][j].setBackground(red);
                     board[i][j].setBorder(BorderFactory.createLineBorder(red));
+                    board[i][j].removeActionListener(this);
                 }
             }
         }
