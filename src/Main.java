c94e07241232e6c26c6859424c312e5b4072a4b1
wkpc <145634104+wkpc@users.noreply.github.com>
2025-02-15 15:19:27 -0700

    - added main for testing

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//testing
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        int[][] expected = new int[][]{{0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}};
+        System.out.println(expected.length);
+        System.out.println(expected[0].length);
+    }
+}
