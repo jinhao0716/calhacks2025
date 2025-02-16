6b7a1a543d59712ee45995d2cfc2d07173febc69
wkpc <145634104+wkpc@users.noreply.github.com>
2025-02-15 20:48:43 -0700

    - added checkMove() function - edited collectMove() and play() function

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,12 @@ import java.util.Scanner;
 
 public class Game
 {
+    ///////KEY///////
+    /// 1 = player 1 land
+    /// 2 = player 2 land
+    /// 3 = neutral middle island land
+    /// ab (a = player number, b = tile number)
+
     //stats
     private static final int ROWS = 10;
     private static final int COLUMNS = 20;
@@ -43,12 +49,24 @@ public class Game
             controls[1] = "A";
             controls[2] = "S";
             controls[3] = "D";
+        }else
+        {
+            controls[0] = "I";
+            controls[1] = "J";
+            controls[2] = "K";
+            controls[3] = "L";
         }
 
+        //keep track if they moved
+        boolean moved = false;
 
-        //move the player
-        collectMove(player, curPos, controls);
-
+        //keep pestering them to move until they can't
+        do
+        {
+            //collect the position they want to move to...
+            int[] newPos = collectMove(player, curPos, controls);
+            moved = checkMove(board, newPos, player);
+        }while(!moved);
     }
 
     /**
@@ -72,7 +90,7 @@ public class Game
             for (int y = 0; y < board.length; y++)
             {
                 //if the player is here...
-                if (board[y][x] == player)
+                if (board[y][x] - 10*player > 0)
                 {
                     //note down the position
                     playerX = x;
@@ -102,40 +120,86 @@ public class Game
         //create scanner
         Scanner scan = new Scanner(System.in);
 
+        String keys = "";
+
+        //convert the player's control from array into a string
+        for (int i = 0; i < controls.length; i++)
+        {
+            keys = keys + controls[i];
+        }
+
         boolean moved = true;
 
         //in case user enters invalid input, will keep looping
         do
         {
             //collect user input
-            System.out.println("Player's " + player + " move:");
+            System.out.println("Player's " + player + " move: " + "(" + keys + ")");
             String move = scan.next().toUpperCase();
 
             //then see where they want to move
-            switch (move)
+            if (move.equals(controls[0])) //if go up...
             {
-                case "W": //if go up...
-                    playerNewX = coords[0];
-                    playerNewY = coords[1] - 1;
-                    break;
-                case "A": //if go down...
-                    playerNewX = coords[0] - 1;
-                    playerNewY = coords[1];
-                    break;
-                case "S": //if go left...
-                    playerNewX = coords[0];
-                    playerNewY = coords[1] + 1;
-                    break;
-                case "D": //if go right...
-                    playerNewX = coords[0] + 1;
-                    playerNewY = coords[1];
-                    break;
-                default: //if the user enters invalid input...
-                    moved = false;
+                playerNewX = coords[0];
+                playerNewY = coords[1] - 1;
+                moved = true;
+            }
+            else if (move.equals(controls[2])) //if go down...
+            {
+                playerNewX = coords[0] - 1;
+                playerNewY = coords[1];
+                moved = true;
+            }
+            else if (move.equals(controls[1])) //if go left...
+            {
+                playerNewX = coords[0];
+                playerNewY = coords[1] + 1;
+                moved = true;
+            }
+            else if (move.equals(controls[3])) //if go right...
+            {
+                playerNewX = coords[0] + 1;
+                playerNewY = coords[1];
+                moved = true;
+            }
+            else //if the user enters invalid input...
+            {
+                moved = false;
             }
         }while (!moved);
 
         int[] newCoords = {playerNewX, playerNewY};
         return  newCoords;
+    }
+
+    /**
+     * checks to see if a move is valid, and perform the move if so
+     *
+     * @param board
+     * @param coords
+     * @param player
+     * @return
+     */
+    public static boolean checkMove(int[][] board, int[] coords, int player)
+    {
+        //if any of the moves would go outside of the map, then instantly invalid
+        if (coords[0] < 0 || coords[1] < 1)
+        {
+            return false;
+        }
+
+        //check if it's the tile they're trying to move onto is the same color as them (or a void)...
+        if (board[coords[1]][coords[0]] != player || board[coords[1]][coords[0]] != 0)
+        {
+            //if it's not, then check to see if there is already a player on there (make sure tile # isn't double digits)
+            if (board[coords[1]][coords[0]] - 10 >= 0)
+            {
+                //then move the player
+                board[coords[1]][coords[0]] += (player * 10);
+            }
+        }
+
+        //if reaching this point, then means player has been moved
+        return true;
     }
 }
