ef5da3e86252e32f1f88c71c4e8f8a9959222099
wkpc <145634104+wkpc@users.noreply.github.com>
2025-02-16 04:29:19 -0700

    - reverted to scanner movement

--- src/Game.java
+++ src/Game.java
@@ -1,7 +1,6 @@
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
+import java.util.Scanner;
 
-public class Game implements KeyListener
+public class Game
 {
     ///////KEY///////
     /// 1 = player 1 land
@@ -25,15 +24,8 @@ public class Game implements KeyListener
     private static final int POWERUPSTRENGTH = 2;
     private static final int SUPERPOWERUPSTRENGTH = -1000;
 
-    //counters
     public static int turn = 0;
     public static int stepsTaken = 0;
-
-    //player coords
-    private static int[] curP1Pos;
-    private static int[] curP2Pos;
-    private static int[] newP1Pos;
-    private static int[] newP2Pos;
 
     public static int[][] gameboard = Board.generateBoard(ROWS, COLUMNS);
 
@@ -47,14 +39,27 @@ public class Game implements KeyListener
         Board.generateGoalIslands(gameboard, XBORDER, YBORDER, ISLANDSIZE);
         Board.generateMidIslands(gameboard, XBORDER, ISLANDSIZE, BUFFER, ISLANDSNUMBER);
 
-        //initialize player positions
-        curP1Pos = findPlayer(gameboard, 1);
-        curP2Pos = findPlayer(gameboard, 2);
-
         /////////graphic stuff///////
-        new Graphic(ROWS, COLUMNS, gameboard);
+        new Graphic(ROWS, COLUMNS);
 
+        do
+        {
+            p1Victory = play(gameboard, 1);
+            p2Victory = play(gameboard, 2);
+            printArray(gameboard);
+        }while(!p1Victory && !p2Victory);
+    }
 
+    public static void printArray(int[][] array)
+    {
+        for (int x = 0; x < array.length; x++)
+        {
+            System.out.println("");
+            for( int y = 0; y < array[0].length; y++)
+            {
+                System.out.print(array[x][y]);
+            }
+        }
     }
 
     /**
@@ -62,13 +67,57 @@ public class Game implements KeyListener
      *
      * @param board the board the player is moving on
      * @param player the player doing the moving
+     * @return true if they reached goal, false if didnt
      */
-    /*public static void play(int[][] board, int[] curPos, int[] newPos, int player)
+    public static boolean play(int[][] board, int player)
     {
+        //collect the player's position
+        int[] curPos = findPlayer(board, player);
 
-        moved = checkMove(board, curPos, newPos, player);
+        //set the controls for the player
+        String[] controls = new String[4];
 
-    }*/
+        //if not player 1 and using arrow keys, controls array will be filled with null
+        if (player == 1)
+        {
+            controls[0] = "W";
+            controls[1] = "A";
+            controls[2] = "S";
+            controls[3] = "D";
+
+        }else
+        {
+            controls[0] = "I";
+            controls[1] = "J";
+            controls[2] = "K";
+            controls[3] = "L";
+        }
+
+        //keep track if they moved
+        boolean moved = false;
+
+        //keep pestering them to move until they can't
+        do
+        {
+            //collect the position they want to move to...
+            int[] newPos = collectMove(player, curPos, controls);
+            moved = checkMove(board, curPos, newPos, player);
+        }while(!moved);
+
+        //now update the board
+        Graphic.updateBoard();
+
+        curPos = findPlayer(board, player);
+
+        //check to see if this move placed them onto their goal island
+        if(goalReached(board, curPos, player))
+        {
+            return true;
+        }
+
+        //otherwise return false if didnt reach goal
+        return false;
+    }
 
     /**
      * searches for the coordinates of a player on the board
@@ -91,7 +140,7 @@ public class Game implements KeyListener
             for (int y = 0; y < board.length; y++)
             {
                 //if the player is here...
-                if (board[y][x] - 10*player > 0)
+                if (board[y][x] / 10 == player)
                 {
                     //note down the position
                     playerX = x;
@@ -112,7 +161,7 @@ public class Game implements KeyListener
      * @param controls the controls for the player that is moving (WASD or arrow keys)
      * @return an int array containing the new coords, [x, y]
      */
-    /*public static int[] collectMove(int player, int[] curCoords, String[] controls)
+    public static int[] collectMove(int player, int[] curCoords, String[] controls)
     {
         int playerNewX = -1;
         int playerNewY = -1;
@@ -169,7 +218,7 @@ public class Game implements KeyListener
         }while (!moved);
 
         return new int[]{playerNewX, playerNewY};
-    }*/
+    }
 
     /**
      * checks to see if a move is valid, and perform the move if so
@@ -180,36 +229,43 @@ public class Game implements KeyListener
      * @param player the player doing the moving
      * @return
      */
-    public static void checkMove(int[][] board, int[] curCoords, int[] newCoords, int player)
+    public static boolean checkMove(int[][] board, int[] curCoords, int[] newCoords, int player)
     {
-        //check if any of the moves would go outside of the map...
-        if (newCoords[0] >= 0 && newCoords[1] >= 1 && newCoords[0] < COLUMNS && newCoords[1] < ROWS)
+        //if any of the moves would go outside of the map, then instantly invalid
+        if (newCoords[0] < 0 || newCoords[1] < 1)
         {
-            //check if it's the tile they're trying to move onto is the same color as them (or a void/hazard)...
-            if (board[newCoords[1]][newCoords[0]] != player  && board[newCoords[1]][newCoords[0]] != 0 && board[newCoords[1]][newCoords[0]] != -3)
-            {
-                //if it's not, then check to see if there is already a player on there (make sure tile # isn't double digits)
-                if (!(board[newCoords[1]][newCoords[0]] - 10 >= 0))
-                {
-                    //then check for power ups...
-                    if (board[newCoords[1]][newCoords[0]] == -2)
-                    {
-                        //if there are any pick it up and turn the tile back to normal
-                        board[newCoords[1]][newCoords[0]] = 3;
-                        stepsTaken -= POWERUPSTRENGTH;
-                    }else if (board[newCoords[1]][newCoords[0]] == -2) //then check for SUPER power ups...
-                    {
-                        //if there are any pick it up and turn the tile back to normal
-                        board[newCoords[1]][newCoords[0]] = 3;
-                        stepsTaken -= SUPERPOWERUPSTRENGTH;
-                    }
+            return false;
+        }
 
-                    //then move the player and reset the tile they were previously standing on
-                    board[newCoords[1]][newCoords[0]] += (player * 10);
-                    board[curCoords[1]][curCoords[0]] -= (player * 10);
+        //check if it's the tile they're trying to move onto is the same color as them (or a void/hazard)...
+        if (board[newCoords[1]][newCoords[0]] != player  && board[newCoords[1]][newCoords[0]] != 0 && board[newCoords[1]][newCoords[0]] != -3)
+        {
+            //if it's not, then check to see if there is already a player on there (make sure tile # isn't double digits)
+            if (!(board[newCoords[1]][newCoords[0]] - 10 >= 0))
+            {
+                //then check for power ups...
+                if (board[newCoords[1]][newCoords[0]] == -2)
+                {
+                    //if there are any pick it up and turn the tile back to normal
+                    board[newCoords[1]][newCoords[0]] = 3;
+                    stepsTaken -= POWERUPSTRENGTH;
+                }else if (board[newCoords[1]][newCoords[0]] == -2) //then check for SUPER power ups...
+                {
+                    //if there are any pick it up and turn the tile back to normal
+                    board[newCoords[1]][newCoords[0]] = 3;
+                    stepsTaken -= SUPERPOWERUPSTRENGTH;
                 }
+
+                //then move the player and reset the tile they were previously standing on
+                board[newCoords[1]][newCoords[0]] += (player * 10);
+                board[curCoords[1]][curCoords[0]] -= (player * 10);
+
+                return true;
             }
         }
+
+        //if reaching this point, then means player has not been moved
+        return false;
     }
 
     /**
@@ -232,49 +288,4 @@ public class Game implements KeyListener
         //otherwise return false
         return false;
     }
-
-    /**
-     * checks to see when a button has been pressed, and updates the player positions if necessary
-     *
-     * @param e the (key) event to be processed
-     */
-    @Override
-    public void keyPressed(KeyEvent e)
-    {
-        ////////////// FOR PLAYER 1 ///////////
-        //check what direction the player wants to move and update the x y positions
-        if (Character.toString(e.getKeyChar()).equals("w"))
-        {
-            newP1Pos[1] = curP1Pos[1] - 1;
-        }else if (Character.toString(e.getKeyChar()).equals("a")) {
-            newP1Pos[0] = curP1Pos[0] - 1;
-        }else if (Character.toString(e.getKeyChar()).equals("s")) {
-            newP1Pos[1] = curP1Pos[1] + 1;
-        }else if (Character.toString(e.getKeyChar()).equals("d")) {
-            newP1Pos[0] = curP1Pos[1] + 1;
-        }
-
-        ////////////// FOR PLAYER 2 ///////////
-        //check what direction the player wants to move and update the x y positions
-        else if (e.getKeyCode() == KeyEvent.VK_UP)
-        {
-            newP2Pos[1] = curP2Pos[1] - 1;
-        }else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            newP2Pos[0] = curP2Pos[0] - 1;
-        }else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-            newP2Pos[1] = curP2Pos[1] + 1;
-        }else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-            newP2Pos[0] = curP2Pos[0] + 1;
-        }
-
-        //now check to see the position the players want to move to is valid and move if it is
-        checkMove(gameboard, curP1Pos, newP1Pos, 1);
-        checkMove(gameboard, curP2Pos, newP2Pos, 2);
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {}
-
-    @Override
-    public void keyTyped(KeyEvent e) {}
 }
